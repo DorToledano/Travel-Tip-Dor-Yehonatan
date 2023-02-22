229473dc493f101507d2e10aabfba2ced56aa8fe
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-02-22 15:37:01 +0200

    hi

--- TravelTip-Dor_Yehonatan/js/app.controller.js
+++ TravelTip-Dor_Yehonatan/js/app.controller.js
@@ -1,6 +1,6 @@
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
-
+//hi dor
 window.onload = onInit
 window.onAddMarker = onAddMarker
 window.onPanTo = onPanTo
