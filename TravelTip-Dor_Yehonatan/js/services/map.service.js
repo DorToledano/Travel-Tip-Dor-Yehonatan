6c62f70bfc3cc2b08b9989a889d805d3fe4a25a9
Dor <dortoledano210@gmail.com>
2023-02-22 15:32:48 +0200

    first-commit

---
+++ TravelTip-Dor_Yehonatan/js/services/map.service.js
@@ -0,0 +1,63 @@
+export const mapService = {
+    initMap,
+    addMarker,
+    panTo
+}
+
+// Var that is used throughout this Module (not global)
+var gMap
+
+function initMap(lat = 32.0749831, lng = 34.9120554) {
+    console.log('InitMap')
+    return _connectGoogleApi()
+        .then(() => {
+            console.log('google available')
+            gMap = new google.maps.Map(
+                document.querySelector('#map'), {
+                center: { lat, lng },
+                zoom: 15
+            })
+            gMap.addListener("click", (mapsMouseEvent) => { //TODO: make it save the locations and render them
+                const locName = prompt(`What's this location called?`)
+                const loc = JSON.parse(JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2))
+                // saveLocation(locName, loc)
+                new google.maps.Marker({
+                    position: loc,
+                    map: gMap,
+                })
+                const center = new google.maps.LatLng(loc.lat, loc.lng);
+                gMap.panTo(center) //TODO: check if it's good
+                // showUserLocations()
+            })
+            console.log('Map!', gMap)
+        })
+}
+
+function addMarker(loc) {
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: gMap,
+        title: 'Hello World!'
+    })
+    return marker
+}
+
+function panTo(lat, lng) {
+    var laLatLng = new google.maps.LatLng(lat, lng)
+    gMap.panTo(laLatLng)
+}
+
+
+function _connectGoogleApi() {
+    if (window.google) return Promise.resolve()
+    const API_KEY = 'AIzaSyC-2VCeXJPVk5bSEQy5TEZVrn0rp1Mrzwc'
+    var elGoogleApi = document.createElement('script')
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
+    elGoogleApi.async = true
+    document.body.append(elGoogleApi)
+
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve
+        elGoogleApi.onerror = () => reject('Google script failed to load')
+    })
+}
---
+++ TravelTip-Dor_Yehonatan/js/services/places.service.js
@@ -0,0 +1,14 @@
+
+//C- 
+//R - 
+//U - 
+//D -
+//L - 
+
+function savePlace() {
+
+}
+
+function _createPlace(id, name, lat, lng, weather, createdAt, updatedAt) {
+     return { id, name, lat, lng, weather, createdAt, updatedAt }
+}
